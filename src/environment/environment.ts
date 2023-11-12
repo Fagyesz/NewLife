78a0c1aa3d0ec49b73a2462b39f197459c1b18b3
Fagyesz <vinczef.o@gmail.com>
2023-11-12 23:12:48 +0100

    Capcha3 sitekey change

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -8,29 +8,30 @@ export const environment = {
     channelId:"UC3GbgMOUrfVnipHXvXQYFkA",
     /* test id */
     /* channelId:"UCZ13TzL1O4nfC30fxua-tSA" */
-    reCAPTCHA3Site:"6LfqBT0nAAAAAHaIX7iU10XLY-uPhPsl3gX1DZRk",
+    //reCAPTCHA3Site:"6LfqBT0nAAAAAHaIX7iU10XLY-uPhPsl3gX1DZRk",
+    reCAPTCHA3Site:"6LeG5AwpAAAAAElB8Z9GZYthyIb9q9wzCWsbAb3M",
     //apiKey: "AIzaSyBSTNuB88ZpzTKG0_AXe41YaYWLp2ElnLY",
     firebase : {
 
       apiKey: "AIzaSyBSTNuB88ZpzTKG0_AXe41YaYWLp2ElnLY",
-    
+
       authDomain: "newlife-9db00.firebaseapp.com",
-    
+
       databaseURL: "https://newlife-9db00-default-rtdb.europe-west1.firebasedatabase.app",
-    
+
       projectId: "newlife-9db00",
-    
+
       storageBucket: "newlife-9db00.appspot.com",
-    
+
       messagingSenderId: "708224628364",
-    
+
       appId: "1:708224628364:web:812f341acb661d6e6c6da9",
-    
+
       measurementId: "G-K496GHWMXH",
-    
+
     }
-   
+
   };
-  
+
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
