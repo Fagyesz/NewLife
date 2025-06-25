531f263b2d4a5d6b5ff2bd807b65cecac7659378
Vincze Florian <vinczef.o@gmail.com>
2025-06-25 07:50:49 +0200

    feat: Setup Firebase Hosting and GitHub Actions deployment

--- NewLife/src/environments/environment.ts
+++ NewLife/src/environments/environment.ts
@@ -2,12 +2,11 @@ export const environment = {
   production: false,
   firebase: {
     apiKey: "REPLACE_WITH_YOUR_API_KEY", // DO NOT COMMIT REAL API KEYS - Use environment.local.ts
-    authDomain: "newlife-9db00.firebaseapp.com",
-    databaseURL: "https://newlife-9db00-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "newlife-9db00",
-    storageBucket: "newlife-9db00.appspot.com",
-    messagingSenderId: "708224628364",
-    appId: "1:708224628364:web:812f341acb661d6e6c6da9",
-    measurementId: "G-K496GHWMXH"
+    authDomain: "newlife-a8648.firebaseapp.com",
+    projectId: "newlife-a8648",
+    storageBucket: "newlife-a8648.firebasestorage.app",
+    messagingSenderId: "925061673374",
+    appId: "1:925061673374:web:6ae65e6a6874a8abcae68f",
+    measurementId: "G-D8G49NSL7L"
   }
 }; 
