cfc34622a55f82eb19bf61482a5d81076f55059a
Vincze Florian <vinczef.o@gmail.com>
2025-06-30 14:39:59 +0200

    0

--- NewLife/src/environments/environment.template.ts
+++ NewLife/src/environments/environment.template.ts
@@ -8,5 +8,9 @@ export const environment = {
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
     appId: "YOUR_APP_ID",
     measurementId: "YOUR_MEASUREMENT_ID"
+  },
+  liveStream: {
+    workerUrl: "https://YOUR_WORKER_NAME.YOUR_SUBDOMAIN.workers.dev",
+    fallbackUrl: "https://YOUR_WORKER_NAME.YOUR_SUBDOMAIN.workers.dev"
   }
 }; 
